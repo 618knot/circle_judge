a7ce05031a24ad3aa6001fd5b90d12b5839b297b
yud0uhu <60646787+yud0uhu@users.noreply.github.com>
2022-02-14 16:27:59 +0900

    fix: モック関数、TODOなど追記

--- lib/model/api.dart
+++ lib/model/api.dart
@@ -3,6 +3,8 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
+
+// TODO:最終的にはWidget部分はこのクラスに書かず切り出したい
 void main() {
   runApp(MaterialApp(
     home: ApiConnection(),
@@ -17,17 +19,20 @@ class ApiConnection extends StatefulWidget {
 class _ApiConnectionState extends State<ApiConnection> {
   Map data = new Map();
   List userData = [];
+  String startData = "0";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView.builder( //スクロール可能な可変リストを作る
-          itemCount: userData == null ? 0 : userData.length, //受け取る数の定義
-          itemBuilder: (BuildContext context, int index) { //ここに表示したい内容をindexに応じて
-            return Card( //cardデザインを定義:material_design
+        body: ListView.builder(
+          itemCount: userData == null ? 0 : userData.length,
+          // itemCount: startData == null ? 0 : startData.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
               child: Row(
                 children: <Widget>[
                       Text("${userData[index]["first_name"]} ${userData[index]["last_name"]}"),
+                  // Text("${startData}"),
                 ],
               ),
             );
@@ -36,7 +41,18 @@ class _ApiConnectionState extends State<ApiConnection> {
     );
   }
 
-  Future getData() async {
+  // ゲームスタート時に必要なゲームIDを受け取る
+  // Future postStart() async {
+  //   var url = Uri.parse('https://circle-judge-backend.herokuapp.com/start');
+  //   http.Response response = await http.get(url);
+  //   data = json.decode(response.body);
+  //   setState(() {
+  //     startData = data["message"];
+  //   });
+  // }
+
+  // モックAPI
+  Future getUser() async {
     var url = Uri.parse('https://reqres.in/api/users?page=2');
     http.Response response = await http.get(url);
     data = json.decode(response.body); //json->Mapオブジェクトに格納
@@ -45,9 +61,42 @@ class _ApiConnectionState extends State<ApiConnection> {
     });
   }
 
+  // Future postQuestion() async {
+  //   var url = Uri.parse('https://circle-judge-backend.herokuapp.com/question');
+  //   http.Response response = await http.get(url);
+  //   data = json.decode(response.body);
+  //   setState(() {
+  //     questionData = data["data"];
+  //   });
+  // }
+
+  // Future postAnswer() async {
+  //   var url = Uri.parse('https://circle-judge-backend.herokuapp.com/question/answer');
+  //   http.Response response = await http.get(url);
+  //   data = json.decode(response.body);
+  //   setState(() {
+  //     answerData = data["message"];
+  //   });
+  // }
+
+  // Future postResult() async {
+  //   var url = Uri.parse('https://circle-judge-backend.herokuapp.com/result');
+  //   http.Response response = await http.get(url);
+  //   data = json.decode(response.body);
+  //   setState(() {
+  //     questionHistoryData = data["question_history"];
+  //     rankingData = data["ranking"];
+  //   });
+  // }
+
   @override
   void initState() {
     super.initState();
-    getData();
+    getUser();
+    // postStart();
+    // postQuestion();
+    // postAnswer();
+    // postAnswer();
+    // postResult();
   }
 }
