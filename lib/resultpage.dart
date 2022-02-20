b820dfba357edf6464c84f4c289cf094ecbdc8e2
618knot <knot618@gmail.com>
2022-02-20 15:19:53 +0900

    resultpage.dartの追加

---
+++ lib/resultpage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ResultPage extends StatelessWidget {
+  const ResultPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text("aaaaaaaaa"),
+    );
+  }
+}
