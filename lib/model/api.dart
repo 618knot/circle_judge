ead35cc4a00b13d65bfde396f52782dc7bdf5411
yud0uhu <60646787+yud0uhu@users.noreply.github.com>
2022-02-21 14:52:22 +0900

    fix スタート時にバックエンドからゲームIDを取得、回答を返却する関数を実装

--- lib/model/api.dart
+++ lib/model/api.dart
@@ -1,63 +1,72 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
+
 import 'package:hello_world/questiondata.dart';
+import 'package:http/http.dart' as http;
 
 Map data = new Map();
 List userData = [];
 String startData = "0";
-final controller = StreamController<bool>.broadcast();
-  // ゲームスタート時に必要なゲームIDを受け取る
-  // Future postStart() async {
-  //   var url = Uri.parse('https://circle-judge-backend.herokuapp.com/start');
-  //   http.Response response = await http.get(url);
-  //   data = json.decode(response.body);
-  //   setState(() {
-  //     startData = data["message"];
-  //   });
-  // }
 
-StreamController<bool> getController (){
+final controller = StreamController<bool>.broadcast();
+
+StreamController<bool> getController() {
   return controller;
 }
-  // モックAPI
-  Future getQuestion() async {
-    var url = Uri.parse('https://reqres.in/api/users?page=1');
-    http.Response response = await http.get(url);
-    data = json.decode(response.body); //json->Mapオブジェクトに格納
 
-    userData = data["data"];
-    for (var i = 0; i < userData.length; i++) {
-      int id = userData[i]["id"];
-      String sentence = userData[i]["email"];
-      String image = userData[i]["avatar"];
-      QuestionData().set(i, Question(id, sentence, image));
-      print(id);
-      print(sentence+image);
-    }
-    //Map->Listに必要な情報だけ格納
-    controller.sink.add(true);
+// スタート時にPOSTでゲームIDを取得する
+void getGameId() async {
+  var url = Uri.parse('https://circlejudgebackend.herokuapp.com/start');
+  final response =
+      await http.post(url, headers: {'Content-Type': 'application/json'});
+  data = json.decode(response.body);
+  String gameId = data["game_id"];
+  print("gameId:");
+  print(gameId);
+  // TODO:gameIdをreturnする
+}
+
+// モックAPIからカードに描画する情報を取得する
+Future getQuestion() async {
+  var url = Uri.parse('https://reqres.in/api/users?page=1');
+  http.Response response = await http.get(url);
+  data = json.decode(response.body); //json->Mapオブジェクトに格納
+
+  userData = data["data"];
+  for (var i = 0; i < userData.length; i++) {
+    int id = userData[i]["id"];
+    String sentence = userData[i]["email"];
+    String image = userData[i]["avatar"];
+    QuestionData().set(i, Question(id, sentence, image));
+    print(id);
+    print(sentence + image);
   }
+  //Map->Listに必要な情報だけ格納
+  controller.sink.add(true);
+}
 
+// バックエンドにPOSTで回答を返却する
 Future setQuestion() async {
-  var url = Uri.parse('https://circlejudgebackend.herokuapp.com/question/answer');
+  var url =
+      Uri.parse('https://circlejudgebackend.herokuapp.com/question/answer');
   Map<String, String> headers = {'content-type': 'application/json'};
-  String body = json.encode ({"game_id":"8141080a-792a-4b17-ace3-5b5b7927277f", "question_id":1,"result":true});
-  http.Response response = await http.post(url, headers: headers, body:body);
+  // TODO:ここでgame_idとquestion_idとresultはquestiondataのAnswerを参照したい
+  String body = json.encode({
+    "game_id": "8141080a-792a-4b17-ace3-5b5b7927277f",
+    "question_id": 1,
+    "result": true
+  });
+  http.Response response = await http.post(url, headers: headers, body: body);
   data = json.decode(response.body);
   String messageData = data["data"];
   print(messageData);
 }
 
-  //@override
-  void API_Init() async{
-    //super.initState();
-    await getQuestion();
-    await setQuestion();
-    // postStart();
-    // postQuestion();
-    // postAnswer();
-    // postAnswer();
-    // postResult();
-  }
+//@override
+void API_Init() async {
+  await getQuestion();
+}
+
+void GAME_ID_INIT() async {
+  getGameId();
+}
