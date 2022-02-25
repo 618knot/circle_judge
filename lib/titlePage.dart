3c5087befccd0a56fa74c7348e82af22e9077345
ItisNoMatter <702sunnylife@gmail.com>
2022-02-25 10:06:19 +0900

    要素をおいただけ

---
+++ lib/titlePage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class TitlePage extends StatelessWidget {
+  Widget Title(){
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Text("サークル診断アプリ",style: TextStyle(fontSize: 40),),
+    );
+  }
+  Widget StartButton(){
+    return ElevatedButton(
+        onPressed: (){},
+        child: Container(
+          padding: EdgeInsets.symmetric(),
+          child: Text("始める"),
+        )
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('wallpaper.jpeg'),
+            fit:BoxFit.cover,
+          )
+        ),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Spacer(flex: 1,),
+              Title(),
+              ElevatedButton(onPressed:(){},child: Text("始める")),
+              Spacer(flex: 2,),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
