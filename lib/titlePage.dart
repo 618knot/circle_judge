a331300903a3bcbd26ebd7770e827def7a031975
yud0uhu <60646787+yud0uhu@users.noreply.github.com>
2022-03-08 17:53:50 +0900

    add ボタンクリックでページ遷移する処理を実装

--- lib/titlePage.dart
+++ lib/titlePage.dart
@@ -1,44 +1,56 @@
 import 'package:flutter/material.dart';
 
 class TitlePage extends StatelessWidget {
-  Widget Title(){
+  Widget Title() {
     return Container(
       padding: EdgeInsets.all(10),
-      child: Text("サークル診断アプリ",style: TextStyle(fontSize: 40),),
+      child: Text(
+        "サークル診断アプリ",
+        style: TextStyle(fontSize: 40),
+      ),
     );
   }
-  Widget StartButton(){
+
+  Widget StartButton(BuildContext context) {
     return ElevatedButton(
-        onPressed: (){},
+        onPressed: () {
+          // Navigator.of(context).pushNamed("/judge");
+          print("aaa");
+        },
         child: Container(
           padding: EdgeInsets.symmetric(),
           child: Text("始める"),
-        )
-    );
+        ));
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
+        body: Container(
+      decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('wallpaper.jpeg'),
-            fit:BoxFit.cover,
-          )
+        image: AssetImage('wallpaper.jpeg'),
+        fit: BoxFit.cover,
+      )),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Spacer(
+              flex: 1,
+            ),
+            Title(),
+            Spacer(flex: 1),
+            ElevatedButton(
+                onPressed: () {
+                  print("aaa");
+                  Navigator.of(context).pushNamed("/judge");
+                },
+                child: Text("始める")),
+            Spacer(flex: 3),
+          ],
         ),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Spacer(flex: 1,),
-              Title(),
-              Spacer(flex:1),
-              ElevatedButton(onPressed:(){},child: Text("始める")),
-              Spacer(flex:3),
-            ],
-          ),
-        ),
-      )
-    );
+      ),
+    ));
   }
 }
