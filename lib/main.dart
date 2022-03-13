ba177f1d53bad7878858363d37d07fe753505f2f
yud0uhu <60646787+yud0uhu@users.noreply.github.com>
2022-03-13 15:10:36 +0900

    add スタート時のローディングダイアログを実装

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,13 @@ class MyApp extends StatelessWidget {
       // home: JudgeProcessPage(),//ExampleHomePage(),
       home: TitlePage(),
       routes: <String, WidgetBuilder>{
-        '/title': (BuildContext context) => new TitlePage(),
-        '/judge': (BuildContext context) => new JudgeProcessPage(),
-        '/result': (BuildContext context) => new ResultPage(),
+        '/title': (BuildContext context) => TitlePage(),
+        '/judge': (BuildContext context) => JudgeProcessPage(),
+        '/result': (BuildContext context) => ResultPage(),
       },
     );
   }
-}r
+}
 
 class ExampleHomePage extends StatefulWidget {
   @override
