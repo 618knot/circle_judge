3ba94190cb58dd76d219736f75cb1b0300395df3
yud0uhu <60646787+yud0uhu@users.noreply.github.com>
2022-03-30 10:16:22 +0900

    Fix headerクラスを切り出ししbottomNavigationBarとして位置を固定

--- lib/resultpage.dart
+++ lib/resultpage.dart
@@ -4,9 +4,9 @@ import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
+import 'package:hello_world/header.dart';
 import 'package:hello_world/model/api.dart';
 import 'package:hello_world/resultdata.dart';
-import 'package:new_gradient_app_bar/new_gradient_app_bar.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_extend/share_extend.dart';
 
@@ -111,161 +111,166 @@ class _ResultPage extends State<ResultPage> {
         print('タップされました');
         detailDialog(context, imageUrl, circleName, introduction);
       },
+      child: Card(
+        margin: EdgeInsets.only(
+          top: 10,
+          right: 20,
+          left: 20,
+        ),
+        color: Colors.yellow,
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Stack(
+                children: [
+                  Container(
+                    color: Colors.teal,
+                    height: 43,
+                    width: MediaQuery.of(context).size.width * matchingRate,
+                  ), //メーター
+
+                  Container(
+                    margin: const EdgeInsets.only(left: 10),
+                    child: Text(
+                      '$circleName',
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.lightGreen,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(
+                    imageUrl,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Flexible(
+                    child: Container(
+                      child: Text(
+                        '$introduction',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                      color: Colors.lightBlue,
+                      height: 110,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
       // child: Card(
-      //   color: Colors.yellow,
-      //   child: Column(
-      //     children: [
-      //       Align(
-      //         alignment: Alignment.topLeft,
-      //         child: Stack(
-      //           children: [
-      //             Container(
-      //               color: Colors.teal,
-      //               height: 43,
-      //               width: MediaQuery.of(context).size.width * matchingRate,
-      //             ), //メーター
+      //     margin: EdgeInsets.only(
+      //       top: 20,
+      //       right: 20,
+      //       left: 20,
+      //     ),
+      //     shape: RoundedRectangleBorder(
+      //       borderRadius: BorderRadius.circular(5),
+      //     ),
+      //     color: Theme.of(context).primaryColor,
+      //     child: Column(
+      //       children: [
+      //         Align(
+      //           alignment: Alignment.topLeft,
+      //           child: Stack(
+      //             children: [
+      //               // TODO:メーターをタイトルボックスの真下に移植させたい
+      //               // CSSパラメータ参考:
+      //               // background: linear-gradient(90deg, #7BD4F1 38.55%, rgba(34, 15, 96, 0) 97.06%), #220F60;
+      //               // border-radius: 0px;
       //
-      //             Container(
-      //               margin: const EdgeInsets.only(left: 10),
-      //               child: Text(
-      //                 '$circleName',
-      //                 style: TextStyle(
-      //                   fontSize: 30,
-      //                 ),
-      //               ),
-      //             ),
-      //           ],
-      //         ),
-      //       ),
-      //       Container(
-      //         color: Colors.lightGreen,
-      //         child: Row(
-      //           crossAxisAlignment: CrossAxisAlignment.start,
-      //           children: [
-      //             Image.network(
-      //               imageUrl,
-      //               width: 100,
-      //               height: 100,
-      //             ),
-      //             Flexible(
-      //               child: Container(
+      //               // Container(
+      //               // color: Colors.teal,
+      //               // height: 43,
+      //               // width: MediaQuery.of(context).size.width * matchingRate,
+      //               // ), //メーター
+      //               // TODO: グラデーション
+      //               Container(
+      //                 padding: const EdgeInsets.all(3.0),
+      //                 margin: const EdgeInsets.only(left: 80, top: 1),
       //                 child: Text(
-      //                   '$introduction',
+      //                   '$circleName',
       //                   style: TextStyle(
-      //                     fontSize: 15,
+      //                     color: Colors.white,
+      //                     fontWeight: FontWeight.bold,
+      //                     fontSize: 12,
       //                   ),
       //                 ),
-      //                 color: Colors.lightBlue,
-      //                 height: 110,
       //               ),
-      //             )
-      //           ],
+      //               Container(
+      //                 padding: const EdgeInsets.all(3.0),
+      //                 margin: const EdgeInsets.only(left: 10, top: 1),
+      //                 decoration: BoxDecoration(
+      //                   borderRadius: BorderRadius.circular(30),
+      //                   color: Colors.white,
+      //                 ),
+      //                 child: Text(
+      //                   '1位',
+      //                   style: TextStyle(
+      //                     color: Theme.of(context).primaryColor,
+      //                     fontSize: 12,
+      //                   ),
+      //                 ),
+      //               ),
+      //               Container(
+      //                 padding: const EdgeInsets.all(3.0),
+      //                 margin: const EdgeInsets.only(left: 240, top: 1),
+      //                 decoration: BoxDecoration(
+      //                   borderRadius: BorderRadius.circular(30),
+      //                   color: Colors.white,
+      //                 ),
+      //                 child: Text(
+      //                   'おすすめ度' + (matchingRate.round() * 100).toString() + '%',
+      //                   style: TextStyle(
+      //                     color: Theme.of(context).primaryColor,
+      //                     fontSize: 12,
+      //                   ),
+      //                 ),
+      //               ),
+      //             ],
+      //           ),
       //         ),
-      //       ),
-      //     ],
-      //   ),
-      // ),
-      child: Card(
-          margin: EdgeInsets.only(
-            top: 20,
-            right: 20,
-            left: 20,
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(5),
-          ),
-          color: Theme.of(context).primaryColor,
-          child: Column(
-            children: [
-              Align(
-                alignment: Alignment.topLeft,
-                child: Stack(
-                  children: [
-                    // TODO:メーターをタイトルボックスの真下に移植させたい
-                    // CSSパラメータ参考:
-                    // background: linear-gradient(90deg, #7BD4F1 38.55%, rgba(34, 15, 96, 0) 97.06%), #220F60;
-                    // border-radius: 0px;
-
-                    // Container(
-                    // color: Colors.teal,
-                    // height: 43,
-                    // width: MediaQuery.of(context).size.width * matchingRate,
-                    // ), //メーター
-                    // TODO: グラデーション
-                    Container(
-                      padding: const EdgeInsets.all(3.0),
-                      margin: const EdgeInsets.only(left: 80, top: 1),
-                      child: Text(
-                        '$circleName',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(3.0),
-                      margin: const EdgeInsets.only(left: 10, top: 1),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.white,
-                      ),
-                      child: Text(
-                        '1位',
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(3.0),
-                      margin: const EdgeInsets.only(left: 240, top: 1),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.white,
-                      ),
-                      child: Text(
-                        'おすすめ度' + (matchingRate.round() * 100).toString() + '%',
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(3.0),
-                color: Colors.white,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Image.network(
-                      imageUrl,
-                      width: 100,
-                      height: 100,
-                    ),
-                    Flexible(
-                      child: Container(
-                        child: Text(
-                          '$introduction',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
-                        color: Colors.white,
-                        height: 110,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          )),
+      //             Container(
+      //               padding: const EdgeInsets.all(3.0),
+      //               color: Colors.white,
+      //               child: Row(
+      //                 crossAxisAlignment: CrossAxisAlignment.start,
+      //                 children: [
+      //                   Image.network(
+      //                     imageUrl,
+      //                     width: 100,
+      //                     height: 100,
+      //                   ),
+      //                   Flexible(
+      //                     child: Container(
+      //                       child: Text(
+      //                         '$introduction',
+      //                         style: TextStyle(
+      //                           fontSize: 15,
+      //                           color: Theme.of(context).primaryColor,
+      //                         ),
+      //                       ),
+      //                       color: Colors.white,
+      //                       height: 110,
+      //                     ),
+      //                   )
+      //                 ],
+      //               ),
+      //             ),
+      //           ],
+      //         )),
     );
   }
 
@@ -278,7 +283,7 @@ class _ResultPage extends State<ResultPage> {
       },
       child: Card(
           margin: EdgeInsets.only(
-            top: 20,
+            top: 10,
             right: 20,
             left: 20,
           ),
@@ -380,45 +385,40 @@ class _ResultPage extends State<ResultPage> {
     );
   }
 
-  // TODO: margin調整
   Widget buttons(context) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Container(
-        height: 90,
-        margin: const EdgeInsets.only(top: 33),
-        color: Theme.of(context).primaryColor,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.white,
-                shape: const CircleBorder(),
-              ),
-              onPressed: () => shareImageAndText('result', shareKey),
-              child: const Icon(
-                Icons.share,
+    return Container(
+      height: 90,
+      color: Theme.of(context).primaryColor,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.white,
+              shape: const CircleBorder(),
+            ),
+            onPressed: () => shareImageAndText('result', shareKey),
+            child: const Icon(
+              Icons.share,
+              color: Colors.black,
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.of(context).pushNamed("/title");
+            },
+            child: const Text(
+              'タイトルへ',
+              style: TextStyle(
                 color: Colors.black,
+                fontSize: 12,
               ),
             ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.white,
-              ),
-              onPressed: () {
-                Navigator.of(context).pushNamed("/title");
-              },
-              child: const Text(
-                'タイトルへ',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -474,47 +474,35 @@ class _ResultPage extends State<ResultPage> {
     return RepaintBoundary(
       key: shareKey,
       child: Scaffold(
-          appBar: NewGradientAppBar(
-            title: const Text('診断結果',
-                style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold)),
-            gradient: LinearGradient(
-              colors: [
-                Color(0xff7BD4F1).withOpacity(0.65),
-                Color(0xff220f60).withOpacity(1.0)
+        appBar: Header(),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                firstCard(
+                    context,
+                    resultLoad(0).circlename,
+                    resultLoad(0).percent,
+                    resultLoad(0).circle_description,
+                    resultLoad(0).circle_image_url),
+                secondThirdCard(
+                    context,
+                    resultLoad(1).circlename,
+                    resultLoad(1).percent,
+                    resultLoad(1).circle_description,
+                    resultLoad(1).circle_image_url),
+                secondThirdCard(
+                    context,
+                    resultLoad(2).circlename,
+                    resultLoad(2).percent,
+                    resultLoad(2).circle_description,
+                    resultLoad(2).circle_image_url)
               ],
             ),
           ),
-          body: SafeArea(
-            child: Center(
-              child: Column(
-                children: [
-                  // header(),
-                  firstCard(
-                      context,
-                      resultLoad(0).circlename,
-                      resultLoad(0).percent,
-                      resultLoad(0).circle_description,
-                      resultLoad(0).circle_image_url),
-                  secondThirdCard(
-                      context,
-                      resultLoad(1).circlename,
-                      resultLoad(1).percent,
-                      resultLoad(1).circle_description,
-                      resultLoad(1).circle_image_url),
-                  secondThirdCard(
-                      context,
-                      resultLoad(2).circlename,
-                      resultLoad(2).percent,
-                      resultLoad(2).circle_description,
-                      resultLoad(2).circle_image_url),
-                  buttons(context),
-                ],
-              ),
-            ),
-          )),
+        ),
+        bottomNavigationBar: buttons(context),
+      ),
     );
   }
 
